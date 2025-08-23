785569185fd61e5bf37772a582daba9de3b5fbe3
Martinho Macovere Domingos <145111230+34sea@users.noreply.github.com>
2025-08-23 17:35:02 +0200

    gfd

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ import DatasUser from './views/Client/DatasUser.vue';
 import settingsuser from './views/Client/settingsuser.vue';
 
 const routes = [
-  { path: '/', component: ProductsClientes },
+  { path: '/', component: Login },
   { path: '/about', component: About },
   { path: '/login', component: Login },
   { path: '/sigup', component: Sigup },
